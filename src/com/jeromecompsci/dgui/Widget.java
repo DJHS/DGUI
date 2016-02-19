b3a205427a8286af7b6d8267a21e0f5dec9d92e0
wangdderek <wangdderek@outlook.com>
2016-02-19 14:01:43 -0500

    Add methods on Widget to set width and height independently of each other.

--- src/com/jeromecompsci/dgui/Widget.java
+++ src/com/jeromecompsci/dgui/Widget.java
@@ -17,4 +17,32 @@ public abstract class Widget {
         getInternal().setMinimumSize(d);
         getInternal().setPreferredSize(d);
     }
+
+    public void setWidth(int width) {
+        Dimension s = getInternal().getSize();
+        s.width = width;
+        getInternal().setSize(s);
+
+        Dimension m = getInternal().getMinimumSize();
+        m.width = width;
+        getInternal().setMinimumSize(m);
+
+        Dimension p = getInternal().getPreferredSize();
+        p.width = width;
+        getInternal().setPreferredSize(p);
+    }
+
+    public void setHeight(int height) {
+        Dimension s = getInternal().getSize();
+        s.height = height;
+        getInternal().setSize(s);
+
+        Dimension m = getInternal().getMinimumSize();
+        m.height = height;
+        getInternal().setMinimumSize(m);
+
+        Dimension p = getInternal().getPreferredSize();
+        p.height = height;
+        getInternal().setPreferredSize(p);
+    }
 }
